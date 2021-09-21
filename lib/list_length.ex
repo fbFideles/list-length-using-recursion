b5baee86d85ba8687344bf8d504b76823f7664e0
fbFideles <felipebreno106@gmail.com>
2021-09-21 22:49:33 +0000

    implementing list length using tail call optimization

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    length(list, 0)
+  end
+
+  defp length([_ | tail], count) do
+    count = count + 1
+    length(tail, count)
+  end
+
+  defp length([], count) do
+    count
   end
 end
